1efd71b9d69764b10899db9d224ef68853b01beb
lixiaochi <ajsdkl@163.com>
2013-04-08 15:34:07 +0800

    Updata JS

--- js/bubble.js
+++ js/bubble.js
@@ -1,5 +1,5 @@
 $(function(){
-	//new Bubble Canvas
+	//Canvas ProToType
 	function Canvas(param)
 	{
 		this.obj = param.obj,
@@ -8,21 +8,21 @@ $(function(){
 
 
 	Canvas.prototype = {
-		//线的颜色
+		//笔触颜色
 		strokeStyle : function(style)
 		{
 			this.context.strokeStyle = style;
 			return this;
 		},
 
-		//线的宽度
+		//笔触宽度
 		lineWidth : function(number)
 		{
 			this.context.lineWidth = number;
 			return this;
 		},
 
-		//设置填充颜色
+		//设置颜色
 		fillStyle : function(style)
 		{
 			this.context.fillStyle = style;
@@ -54,6 +54,27 @@ $(function(){
 		stroke : function()
 		{
 			this.context.stroke();
+			return this;
+		},
+
+		//位移图形
+		translate : function(x,y)
+		{
+			this.context.translate(x,y);
+			return this;
+		},
+
+		//缩放图形
+		scale : function(x,y)
+		{
+			this.context.scale(x,y);
+			return this;
+		},
+
+		//旋转图形
+		rotate : function(number)
+		{
+			this.context.rotate(number)
 			return this;
 		},
 
@@ -107,6 +128,57 @@ $(function(){
 		{
 			this.context.arc(x,y,radius,startAngle,endAngle,anticlockwise)
 			return this;
+		},
+
+
+
+
+		//文本设置
+		font : function(style)
+		{
+			this.context.font = style;
+			return this;
+		},
+
+		//绘制文本并填充
+		fillText : function(text,x,y)
+		{
+			this.context.fillText(text,x,y);
+			return this;
+		},
+
+		//绘制只有边框的文本
+		strokeText : function(text,x,y)
+		{
+			this.context.strokeText(text,x,y);
+			return this;
+		},
+
+
+
+
+		//保存画布
+		save : function()
+		{
+			this.context.save();
+			return this;
+		},
+
+		//恢复画布
+		restore : function()
+		{
+			this.context.restore();
+			return this;
+		},
+
+
+
+
+		//重置画布
+		resetCanvas : function()
+		{
+			this.obj.attr('width',this.obj.width());
+			this.obj.attr('height',this.obj.height());
 		}
 	}
 
@@ -119,6 +191,18 @@ $(function(){
 	canvas.strokeStyle('#FF0000').lineWidth(1).strokeRect(0,0,50,50);
 	canvas.beginPath().lineWidth(10).moveTo(40,40).lineTo(340,40).closePath().stroke();
 	canvas.beginPath().arc(200,200,100,0,Math.PI/2,false).lineTo(200,200).closePath().fill();
+	canvas.strokeStyle('').lineWidth(1).font('italic 50px serif').strokeText('HELLO',30,30);
+	canvas.resetCanvas();
+	canvas.fillStyle('#FFF000').save().fillRect(50,50,100,100);
 
+	canvas.restore().fillRect(100,100,100,100);
 
+	canvas.fillStyle('#FF0000').translate(50,50).fillRect(300,300,100,100);
+
+	canvas.fillStyle('#FF0000').fillRect(0,0,100,100);
+	canvas.resetCanvas();
+
+	canvas.save()
+		  .translate(100,100).scale(2,2).fillRect(0,0,100,100)
+		  .restore().rotate(Math.PI/4).fillRect(0,0,100,100);
 })
